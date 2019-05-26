bdcdd01175afe966124cfb488e4797b5e3667a5a
Luan Alves <32730441+luannsr12@users.noreply.github.com>
2019-05-26 11:22:19 -0300

    Update calcula.php

--- calcula.php
+++ calcula.php
@@ -1,11 +1,8 @@
 <?php
-    /* O código abaixo foi desenvolvido na vídeo aula, na qual eu encino com usar a Api dos correios, para calcular o frete de um determinado produto
+    /* O código abaixo foi desenvolvido na vídeo aula, na qual eu ensino com usar a Api dos correios, para calcular o frete de um determinado produto
      * Video: https://www.youtube.com/watch?v=61-klmvyUvA
      * www.scriptmundo.com
-     * = Este código foi atualizado (06/02/2019) , portando algumas coisas estão diferentes da video
-     * Agradeço a todos que assistiram o vídeo
-     * Gostaria de pedir para que deizasse uma "STAR" neste repositorio
-     * Obrigado
+     * = Este código foi atualizado (06/02/2019) , portando algumas coisas estão diferentes do video
      */
 
 
